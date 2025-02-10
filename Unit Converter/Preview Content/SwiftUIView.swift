e82ff043c7b84cc64c0cf2cfb3148d7ce081803c
Marshal <167097013+Kingmarshal321@users.noreply.github.com>
2025-02-10 16:23:34 +0200

    Decipher depending on  the unit to be converted

---
+++ Unit Converter/Preview Content/SwiftUIView.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIView.swift
+//  Unit Converter
+//
+//  Created by Marshal on 2/10/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+
