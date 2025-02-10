34de54417f9e3d1822e52f7bc4349a8d68f03233
tanakakufa <167096897+tanakakufa@users.noreply.github.com>
2025-02-10 12:58:11 -0800

    Designed the user interface for the App

--- Unit Converter/ContentView.swift
+++ Unit Converter/ContentView.swift
@@ -8,14 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var selectedOption: String = "Select the category"
+    @State private var selectedUnit: String = ""
+    @State private var valueEntered: String = ""
+    @State private var convertTo: String = ""
+var body: some View {
+    
+    VStack {
+        Menu {
+            Button("Option 1", action: { selectedOption = "Option 1" })
+            Button("Option 2", action: { selectedOption = "Option 2" })
+            Button("Option 3", action: { selectedOption = "Option 3" })
+        } label: {
+            Label(selectedOption, systemImage: "chevron.down")
+                .padding()
+                .background(Color.blue.opacity(0.1))
+                .cornerRadius(8)
+            
+            
         }
         .padding()
+        HStack {
+            TextField("", text: $valueEntered)
+                .padding()
+                .background(Color.blue.opacity(0.1))
+                .cornerRadius(10)
+                .padding()
+            
+            Menu {
+                Button("unit option 1", action: {
+                    selectedUnit = "unit option 1"
+                })
+                Button("unit option 2",action:{
+                    selectedUnit = "unit option 2"})
+            } label: {
+                Label(selectedUnit, systemImage: "chevron.down")
+                    .padding()
+                    .background(Color.blue.opacity(0.1))
+                    .cornerRadius(8)
+            }
+        }
+
+        Spacer()
+        
+    }
+    .background(Color.pink.opacity(0.5))
+    
+    VStack{
+        
+        Menu {
+            Button("option1"){
+                
+            }
+        } label: {
+            Label("convert to:", systemImage: "chevron.down")
+        }
+        .padding()
+        Spacer()
+        
+        HStack {
+            
+            TextField("", text: $convertTo)
+                .padding()
+                .background(Color.pink.opacity(0.1))
+                .cornerRadius(8)
+                .padding()
+            
+            
+        }
+    }
+    .background(Color.blue.opacity(0.5))
     }
 }
 
