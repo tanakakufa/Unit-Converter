27c1fd0e91898b1b0201cf573427e53c702d6314
tanakakufa <167096897+tanakakufa@users.noreply.github.com>
2025-02-11 01:28:58 -0800

    Revert "Button functionality for unit conversion"

--- Unit Converter/Preview Content/SwiftUIView.swift
+++ Unit Converter/Preview Content/SwiftUIView.swift
@@ -7,11 +7,11 @@
 
 import SwiftUI
 
-struct Conversion: View {
+struct SwiftUIView: View {
     var body: some View {
-     
-        
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-  
 }
+
+
+
